f581efe488a0ceb0bea09b7e198d5a620a0cc849
msaadirfan987-creator <msaadirfan987@gmail.com>
2026-07-31 20:36:28 +0500

    admin dashboard update

--- client/E-comerce/src/pages/AuthPage.jsx
+++ client/E-comerce/src/pages/AuthPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import logo from '../assets/logo.png';
 import { useAuth } from '../context/AuthContext';
 
